88b52bfa99d160ba83b49ac5d89a3ee21eba222e
Kristofer Sommestad <kristofer.sommestad@springworks.se>
2015-10-15 16:11:42 +0200

    Fixed invalid reference to { child_process }

--- src/git/git-service.js
+++ src/git/git-service.js
@@ -41,7 +41,7 @@ internals.getLatestAuthorName = function({ child_process }) {
 };
 
 
-internals.executeCommand = function({ child_process }, git_command_args) {
+internals.executeCommand = function(child_process, git_command_args) {
   return new Promise((resolve, reject) => {
     const spawned_process = child_process.spawn('git', git_command_args);
     let stdout_str = '';
